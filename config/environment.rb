14058a4a7183adbdebaabec46bbb08706704901e
Ben Hoskings <ben@hoskings.net>
2009-05-18 14:26:34 +1000

    Added rubyosa gem requirement.

--- config/environment.rb
+++ config/environment.rb
@@ -4,6 +4,7 @@ require File.join(File.dirname(__FILE__), 'boot')
 Rails::Initializer.run do |config|
   config.gem 'hammock', :version => '~> 0.2.24'
   config.gem 'haml'
+  config.gem 'rubyosa', :lib => 'rbosa'
 
   config.time_zone = 'UTC'
 end
