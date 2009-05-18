dffee1448c95c2d0bb9177075d6e7ef305a918d4
Ben Hoskings <ben@hoskings.net>
2009-05-18 21:53:38 +1000

    Added layout.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,9 @@
 class ApplicationController < ActionController::Base
   include Hammock::RestfulActions
 
-  helper :all # include all helpers, all the time
-  protect_from_forgery # See ActionController::RequestForgeryProtection for details
+  helper :all
+  layout 'application'
+  protect_from_forgery
 
   def current_user
     nil
