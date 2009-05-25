68830ce160b540d9c05dfa0028800b0b43045a95
Ben Hoskings <ben@hoskings.net>
2009-05-25 22:54:11 +1000

    Added Event and Vote associations to Entry.

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -3,6 +3,14 @@ class Entry < ActiveRecord::Base
   public_resource
 
   belongs_to :track
+
+  has_many :votes
+
+  has_many :events
+  has_many :add_events
+  has_many :play_events
+  has_many :vote_events
+
   delegate :play!, :to => :track
 
   named_scope :upcoming, :conditions => 'played_at IS NULL', :order => 'created_at DESC'
