358f9e710c8a3f60a259db50c70b9f59d560e1b2
Ben Hoskings <ben@hoskings.net>
2009-05-25 23:24:50 +1000

    Added domready block to application.js, with hoverable/child logic.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,1 +1,13 @@
 var KEYS = { BACKSPACE:8, TAB:9, RETURN:13, ENTER:3, ESC:27, SPACE:32, LEFT:37, UP:38, RIGHT:39, DOWN:40, DELETE:46 };
+
+$(function() {
+
+  // show when hovered
+  jQuery('.hoverable .child').css({opacity: 0});
+  jQuery('.hoverable').hover(function() {
+    jQuery('.child', this).animate({opacity: 1}, 100);
+  }, function() {
+    jQuery('.child', this).animate({opacity: 0}, 400);
+  });
+
+});
