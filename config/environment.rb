1ca84ab7da3feb7c01dd8cf320c405bd94d2df92
Ben Hoskings <ben@hoskings.net>
2009-05-25 23:37:55 +1000

    Bumped hammock dep to 0.3.0.

--- config/environment.rb
+++ config/environment.rb
@@ -2,7 +2,7 @@ RAILS_GEM_VERSION = '2.3.2' unless defined? RAILS_GEM_VERSION
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
-  config.gem 'hammock', :version => '~> 0.2.24'
+  config.gem 'hammock', :version => '~> 0.3.0'
   config.gem 'haml'
   config.gem 'rubyosa', :lib => 'rbosa'
 
