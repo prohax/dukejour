84001c3ca374b2da7c72a28c7671bda617040a41
Ben Hoskings <ben@hoskings.net>
2009-05-26 01:29:18 +1000

    Updated event polling - handling JSON responses properly now.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -3,22 +3,28 @@ var KEYS = { BACKSPACE:8, TAB:9, RETURN:13, ENTER:3, ESC:27, SPACE:32, LEFT:37, 
 var EventCheckInterval = 10;
 
 function checkForEvents() {
-  console.log("checking for events.");
-
-  $.get(
-    '/events/window',
-    {
-      window_size: EventCheckInterval,
-      format: 'json'
-    },
-    function(data) {
-      $(data).each(function(elem) {
-        console.log(elem);
-      });
+  $.getJSON(
+    '/events/window?format=json&window_size=' + EventCheckInterval,
+    function(data, textStatus) {
+      if ('success' == textStatus) {
+        $.each(data, function(i, e){
+          handleEventJSON(e);
+        });
+      }
     }
   );
-
   setTimeout(checkForEvents, EventCheckInterval * 1000);
+}
+
+function handleEventJSON(e) {
+  if (e.vote_event) {
+    vote_count = $('ul.entries li.entry_' + e.vote_event.entry_id + ' .votes .count');
+    vote_count
+      .show('highlight', 2000)
+      .html(parseInt(vote_count.html()) + 1);
+  } else if (e.add_event) {
+
+  }
 }
 
 $(function() {
