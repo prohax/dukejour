528b6f8efd248b6280e911db5d22c63186262462
Ben Hoskings <ben@hoskings.net>
2009-10-01 00:32:54 +1000

    Updated rails and hammock versions.

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,8 @@
-RAILS_GEM_VERSION = '2.3.2' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.4' unless defined? RAILS_GEM_VERSION
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
-  config.gem 'hammock', :version => '~> 0.3.0'
+  config.gem 'hammock', :version => '~> 0.3.9'
   config.gem 'haml'
   config.gem 'rubyosa', :lib => 'rbosa'
 
