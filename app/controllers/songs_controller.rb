e0c282e5d00ac74f710e115dafe9949df130ab4a
Ben Hoskings <ben@hoskings.net>
2009-10-05 04:24:48 +1100

    Set song suggest order to artist, album, disc_number, track_number, name.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,7 @@
 class SongsController < ApplicationController
+
+  before_suggest 'set order' do
+    @order = %w[songs.normalized_artist songs.normalized_album songs.disc_number songs.track_number songs.normalized_name]
+  end
+
 end
