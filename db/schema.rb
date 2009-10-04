32e924ef2adb5cbb905bda558a25beec4cda2bb5
Ben Hoskings <ben@hoskings.net>
2009-10-04 16:02:41 +1100

    Updated schema after migrating the votes table away.

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20091003071751) do
+ActiveRecord::Schema.define(:version => 20091003085956) do
 
   create_table "entries", :force => true do |t|
     t.integer  "track_id"
@@ -42,12 +42,6 @@ ActiveRecord::Schema.define(:version => 20091003071751) do
     t.string   "album",         :limit => 256
     t.string   "name",          :limit => 256
     t.integer  "year"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "votes", :force => true do |t|
-    t.integer  "entry_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
