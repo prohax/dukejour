d74c600001774d72c832943b007b6264699e8b4f
Ben Hoskings <ben@hoskings.net>
2009-10-27 02:42:08 +1100

    Added juggernaut hook for added entries.

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -2,6 +2,7 @@ class EntriesController < ApplicationController
 
   after_create "create add event" do
     @entry.add_events.create
+    call_via_juggernaut :add_event, render(:partial => 'index_entry', :locals => {:entry => @entry}).inspect
   end
 
   def vote
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,5 +1,11 @@
 var KEYS = { BACKSPACE:8, TAB:9, RETURN:13, ENTER:3, ESC:27, SPACE:32, LEFT:37, UP:38, RIGHT:39, DOWN:40, DELETE:46 };
 
+function add_event(entry) {
+  $('#queue ul.entries').append(entry)
+    .children(':last').hide()
+    .show('blind', {direction: 'vertical'}, 500);
+}
+
 function vote_event(entry) {
   entry = entry.entry
   vote_count = $('ul.entries li.entry_' + entry.id + ' .votes .count');
