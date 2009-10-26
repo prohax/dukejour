530580d473a7597c43c722edb8a21228883f76c1
Ben Hoskings <ben@hoskings.net>
2009-10-27 03:14:03 +1100

    Voting re-sorts the list now.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -7,11 +7,17 @@ function add_event(entry) {
 }
 
 function vote_event(entry) {
-  entry = entry.entry
-  vote_count = $('ul.entries li.entry_' + entry.id + ' .votes .count');
-  vote_count
-    .html(parseInt(vote_count.html()) + 1)
-    .stop()
+  entry = entry.entry;
+  vote_count = $('#queue ul.entries li.entry_' + entry.id + ' .votes .count')
+  vote_count.html(parseInt(vote_count.html()) + 1);
+
+  new_list = $('#queue ul.entries li').sort(function(a, b) {
+    return parseInt($(b).find('.votes .count').html())
+      - parseInt($(a).find('.votes .count').html());
+  });
+  $.each(new_list, function(i, item) { $('#queue ul.entries').append(item); });
+
+  $(vote_count).stop()
     .animate({'font-size': '1.5em'}, 200)
     .animate({'font-size': '0.85em'}, 200)
     .animate({'font-size': '1em'}, 200);
