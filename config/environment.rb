c18b1f35f8e5f3478560df2e21339d76b4501c98
Ben Hoskings <ben@hoskings.net>
2010-04-17 17:22:58 +1000

    Updated environment.rb.

--- config/environment.rb
+++ config/environment.rb
@@ -1,38 +1,14 @@
-RAILS_GEM_VERSION = '2.3.4' unless defined? RAILS_GEM_VERSION
-require File.join(File.dirname(__FILE__), 'boot')
+# Load the rails application
+require File.expand_path('../application', __FILE__)
 
-Rails::Initializer.run do |config|
-  config.gem 'hammock', :version => '~> 0.5.4'
-  config.gem 'haml'
-  config.gem 'collectiveidea-delayed_job', :version => '~> 1.8.2', :lib => 'delayed_job'
-
-  config.time_zone = 'UTC'
-end
+# Initialize the rails application
+Rails3::Application.initialize!
 
 if "irb" == $0
   ActiveRecord::Base.logger = Logger.new(STDOUT)
   require 'hirb'
   Hirb.enable
-  
+
   require 'appscript'
   include Appscript
-end
-
-def iTunes
-  ITunesInterface.new
-end
-
-def call_via_juggernaut function, data = "{}"
-  Juggernaut.send_to_channels "#{function}(#{escape_juggernaut data});", ['dukejour']
-end
-
-def escape_juggernaut message
-  message.gsub(/\n/, '\n').gsub(/\r/, '\r')
-end
-
-def juggernaut_message message, opts = {}
-  call_via_juggernaut :message_event, opts.merge({
-    :message => message,
-    :timestamp => Time.now.to_i_msec
-  }).to_json
 end
