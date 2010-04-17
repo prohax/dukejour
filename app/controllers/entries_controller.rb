df2cc8af8d6b414cc03c902920dd79202432026f
Ben Hoskings <ben@hoskings.net>
2010-04-17 20:33:56 +1000

    Added default EntriesController.

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,35 +1,83 @@
 class EntriesController < ApplicationController
+  # GET /entries
+  # GET /entries.xml
+  def index
+    @entries = Entry.all
 
-  before_index "load libraries" do
-    @libraries = Library.all
-  end
-  after_create "load libraries" do
-    @libraries = Library.all
-  end
-
-  after_create "create add event" do
-    @entry.add_events.create :creator => current_user
-  end
-
-  after_create "juggernaut render" do
-    respond_to {|format|
-      format.jug { render_juggernaut :add_event, render(:partial => 'index_entry.html', :locals => {:entry => @entry}).inspect }
-    }
-  end
-
-  def vote
-    if find_record
-      respond_to {|format|
-        if @entry.vote! :creator => current_user
-          format.jug { render_juggernaut :vote_event, @entry.to_json(:include => :song) }
-        else
-          format.jug {
-            juggernaut_message @entry.errors.full_messages.first, :kind => 'error'
-            render :nothing => true
-          }
-        end
-      }
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml  { render :xml => @entries }
     end
   end
 
+  # GET /entries/1
+  # GET /entries/1.xml
+  def show
+    @entry = Entry.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.xml  { render :xml => @entry }
+    end
+  end
+
+  # GET /entries/new
+  # GET /entries/new.xml
+  def new
+    @entry = Entry.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.xml  { render :xml => @entry }
+    end
+  end
+
+  # GET /entries/1/edit
+  def edit
+    @entry = Entry.find(params[:id])
+  end
+
+  # POST /entries
+  # POST /entries.xml
+  def create
+    @entry = Entry.new(params[:entry])
+
+    respond_to do |format|
+      if @entry.save
+        format.html { redirect_to(@entry, :notice => 'Entry was successfully created.') }
+        format.xml  { render :xml => @entry, :status => :created, :location => @entry }
+      else
+        format.html { render :action => "new" }
+        format.xml  { render :xml => @entry.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  # PUT /entries/1
+  # PUT /entries/1.xml
+  def update
+    @entry = Entry.find(params[:id])
+
+    respond_to do |format|
+      if @entry.update_attributes(params[:entry])
+        format.html { redirect_to(@entry, :notice => 'Entry was successfully updated.') }
+        format.xml  { head :ok }
+      else
+        format.html { render :action => "edit" }
+        format.xml  { render :xml => @entry.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /entries/1
+  # DELETE /entries/1.xml
+  def destroy
+    @entry = Entry.find(params[:id])
+    @entry.destroy
+
+    respond_to do |format|
+      format.html { redirect_to(entries_url) }
+      format.xml  { head :ok }
+    end
+  end
 end
