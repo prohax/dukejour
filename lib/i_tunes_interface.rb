94dc8eab5fc5e8a84e47b2f73ce2d5c993ca268c
Ben Hoskings <ben@hoskings.net>
2010-04-18 20:52:53 +1000

    Wrapped ITunesInterface in a module so it can be included and then used with #iTunes.

--- lib/i_tunes_interface.rb
+++ lib/i_tunes_interface.rb
@@ -1,40 +1,44 @@
-class ITunesInterface
-
-  def sources
-    app_reference.sources.get.select {|s|
-      [:library, :shared_library].include? s.kind.get
-    }
-  rescue Exception => ex
-    puts "#{ex.class}: Couldn't connect to iTunes: #{ex.backtrace.first}: #{ex.message}"
-    []
+module ITunes
+  def iTunes
+    ITunesInterface.new
   end
 
-  def player_state
-    app_reference.player_state.get
-  end
+  class ITunesInterface
+    def sources
+      app_reference.sources.get.select {|s|
+        [:library, :shared_library].include? s.kind.get
+      }
+    rescue Exception => ex
+      puts "#{ex.class}: Couldn't connect to iTunes: #{ex.backtrace.first}: #{ex.message}"
+      []
+    end
 
-  def stopped?; :stopped == player_state end
-  def playing?; :playing == player_state end
-  def paused?;  :paused  == player_state end
+    def player_state
+      app_reference.player_state.get
+    end
 
-  def play! obj = nil
-    if obj
-      app_reference.play obj, :once => true, :timeout => 5
-    elsif paused?
-      app_reference.playpause
+    def stopped?; :stopped == player_state end
+    def playing?; :playing == player_state end
+    def paused?;  :paused  == player_state end
+
+    def play! obj = nil
+      if obj
+        app_reference.play obj, :once => true, :timeout => 5
+      elsif paused?
+        app_reference.playpause
+      end
+    end
+    def pause!
+      app_reference.pause
+    end
+    def stop!
+      app_reference.stop
+    end
+
+    private
+
+    def app_reference
+      @app_reference ||= Appscript.app('iTunes')
     end
   end
-  def pause!
-    app_reference.pause
-  end
-  def stop!
-    app_reference.stop
-  end
-
-  private
-
-  def app_reference
-    @app_reference ||= Appscript.app('iTunes')
-  end
-
 end
