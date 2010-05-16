6b57ebbf0e65ac5485245aaadae5050cfb019987
Ben Hoskings <ben@hoskings.net>
2010-05-16 14:44:36 +1000

    cookie_verification_secret -> secret_token upgrade for rails 3.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random, 
 # no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.cookie_verifier_secret = 'a8808aaa60a36e067e8c3e6535b57e828f761b4869827be13146e2df62e018bd2d68dfec4b630d72b31c6082a0d11ccb57fb1e2fc885e0c3d9ba6283fa1a2630'
+Rails.application.config.secret_token = 'e050e4f7e710413f89a5ac7f1b664572716bf4106f8e12fd78b5286616e0c89e623ba5fe86d1e8a0a9ce5b452616b7f7209a87952a4f50d0292283fc350a7b3e'
